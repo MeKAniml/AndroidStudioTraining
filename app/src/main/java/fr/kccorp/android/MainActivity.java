aa3bc216caa30ec126a87c1ee4dc0e5bc336e84e
MeKAniml <m3kaniml@gmail.com>
2022-06-08 19:39:44 +0200

    firstcommit

--- app/src/main/java/fr/kccorp/android/MainActivity.java
+++ app/src/main/java/fr/kccorp/android/MainActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,13 +22,16 @@ public class MainActivity extends AppCompatActivity {
     private Button bouton1;
     private Button bouton2;
     private Button bouton3;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        this.bouton2 = findViewById(R.id.btn2);
         this.bouton1 = findViewById(R.id.btn1);
+        this.editText = findViewById(R.id.editText);
+        this.bouton3 = findViewById(R.id.btn3);
         this.bouton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,21 +40,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        this.bouton2 = findViewById(R.id.btn2);
+
         this.bouton2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                 builder.setCancelable(false);
-                builder.setTitle("Super boite de dialogue");
-                builder.setMessage("Wow c'est super, ce message est vraiment génial");
+                builder.setTitle(getResources().getString(R.string.popup_title));
+                builder.setMessage(getResources().getString(R.string.popup_message));
                 builder.setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         Toast.makeText(MainActivity.this, "Annulé", Toast.LENGTH_SHORT).show();
                     }
                 });
-                builder.setPositiveButton("Suivant", new DialogInterface.OnClickListener() {
+                builder.setPositiveButton(getResources().getString(R.string.popup_btn_oui), new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         finish();
@@ -60,12 +66,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        this.bouton3 = findViewById(R.id.btn3);
         this.bouton3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "View View", Toast.LENGTH_SHORT).show();
+                String contenu = editText.getText().toString();
+                Intent i = new Intent(MainActivity.this, HelloActivity.class);
+                i.putExtra("contenu", contenu);
+                if (contenu.isEmpty()) {
+
+                    editText.setError("Veuillez entrer un contenu");
+                } else {
+                    startActivity(i);
+                }
             }
         });
+
     }
 }
